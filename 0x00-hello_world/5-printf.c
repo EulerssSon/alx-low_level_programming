cfaae6050af097a6fe1b247e90313f273ed6f6bc
EulerssSon <alfy.kerolous@gmail.com>
2023-09-07 02:47:36 -0700

    new task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stido.h>
+
+/**
+ * main - Entry point function
+ *
+ * Return: 0 sucess exit code
+ *
+ * Description: print a sentence
+ */
+int main(void)
+{
+	printf("\nwith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
