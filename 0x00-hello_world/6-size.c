4be9111538c52e0bc34231a51684c9a8847d3657
EulerssSon <alfy.kerolous@gmail.com>
2023-09-07 03:05:05 -0700

    new task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point function
+ *
+ * Return: 0 sucess exit code (Succeeded)
+ *
+ * Description: print the size of the  sentence
+ */
+int main(void)
+{
+        printf("Size of a char: %lu byte(s)\n", sizeof(char));
+        printf("Size of an int: %lu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int ));
+        printf("Size of a float: %lu byte(s)\n", sizeof(float));
+        return (0);
+}
