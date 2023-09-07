b3c08ff112be3629c7de52f15d31cd726396c9ab
EulerssSon <alfy.kerolous@gmail.com>
2023-09-07 03:07:57 -0700

    new task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,18 @@
 /**
  * main - Entry point function
  *
- * Return: 0 sucess exit code (Succeeded)
+ * Return: 0 (Success)
  *
- * Description: print the size of the  sentence
+ * Description: Print the size of different data types
  */
 int main(void)
 {
         printf("Size of a char: %lu byte(s)\n", sizeof(char));
         printf("Size of an int: %lu byte(s)\n", sizeof(int));
         printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int ));
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
         printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
         return (0);
 }
+
