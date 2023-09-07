9cea2645422317ab2eab334e06a6abbe43793cf3
EulerssSon <alfy.kerolous@gmail.com>
2023-09-07 04:28:04 -0700

    new task

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ *main: This is the main functrion
+ *@void: void function returns null
+ *Return: NUll
+ *Description: print hellow rorld
+  */
+int main(void)
+{
+	printf("hello world!");
+	return (0);
+}
