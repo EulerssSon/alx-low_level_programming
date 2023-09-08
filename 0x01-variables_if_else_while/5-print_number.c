75fab3ac0890a29115681fceb59f639cd9d5d685
EulerssSon <alfy.kerolous@gmail.com>
2023-09-08 03:33:34 -0700

    new task

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ *
+ * Description: parint alphabers
+ */
+int main(void)
+{
+char alphabets = 48;
+while (alphabets <= 57)
+{
+putchar(alphabets++);
+}
+putchar(10);
+return (0);
+}
