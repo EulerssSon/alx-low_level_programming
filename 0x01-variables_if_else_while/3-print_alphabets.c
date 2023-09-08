612eea7749c80fb5fd60824045acda5ba8c95800
EulerssSon <alfy.kerolous@gmail.com>
2023-09-08 03:03:41 -0700

    new task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ *
+ * Description: parint alphabers
+ */
+int main(void)
+{
+char alphabets = 'a';
+while (alphabets <= 'z')
+{
+printf("%c", alphabets);
+alphabets++;
+}
+return (0);
+}
