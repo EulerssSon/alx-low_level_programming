a66f7066404a2347998ec88b8c3c0e7116b08fd4
EulerssSon <alfy.kerolous@gmail.com>
2023-09-09 15:14:32 -0700

    new task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,19 +15,21 @@ int e;
 
 for (x = 0; x <= 9;  x++)
 {
-for (y = x+1; y <= 9; y++)
+for (y = 0; y <= 9; y++)
 {
-for (z = y+1; z <= 9; z++)
+for (z = x; z <= 9; z++)
 {
-for(e = z+1; e <= 9; e++)
+for (e = y+1; e <= 9; e++)
 {
-putchar(48 + x);
-putchar(48 + y);
+if (!(x ==9 && y ==8 ))
+{
+putchar(x+48);
+putchar(y+48);
 putchar(' ');
-
-putchar(48 + z);
-putchar (48 + e);
+putchar(z+48);
+putchar(e+48);
 putchar(',');
+}
 }
 }
 }
