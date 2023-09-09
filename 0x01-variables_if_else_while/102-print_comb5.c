334c34866c8c97b73e043fc289339c3e2c8fbefb
EulerssSon <alfy.kerolous@gmail.com>
2023-09-09 14:37:24 -0700

    new task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+# include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ *
+ * Description: unique of 3 nums
+*/
+int main(void)
+{
+int x;
+int y;
+int z;
+int e;
+
+for (x = 0; x <= 9;  x++)
+{
+for (y = x+1; y <= 9; y++)
+{
+for (z = y+1; z <= 9; z++)
+{
+for(e = z+1; e <= 9; e++)
+{
+putchar(48 + x);
+putchar(48 + y);
+putchar(' ');
+
+putchar(48 + z);
+putchar (48 + e);
+putchar(',');
+//if (!(x == 9 && y == 8 && z == 9 && e ==9))
+
+//{
+//putchar(',');
+//}
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
