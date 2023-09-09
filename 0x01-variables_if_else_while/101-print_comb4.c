035545c6785410386834ef48e584bf5f2220b912
EulerssSon <alfy.kerolous@gmail.com>
2023-09-09 13:43:43 -0700

    new task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,9 +14,9 @@ int z;
 
 for (x = 0; x <= 9;  x++)
 {
-for (y = x+1 ; y <= 9; y++)
+for (y = x + 1 ; y <= 9; y++)
 {
-for (z = y+1 ; z <= 9; z++)
+for (z = y + 1 ; z <= 9; z++)
 {
 putchar(48 + x);
 putchar(48 + y);
