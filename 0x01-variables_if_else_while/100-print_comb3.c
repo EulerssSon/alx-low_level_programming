b086ecc7b562e0e91a2b88ed193780e83c87cd84
EulerssSon <alfy.kerolous@gmail.com>
2023-09-09 12:25:32 -0700

    new task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,39 +5,41 @@
  * main - main function
  *
  * Description: print unique from 0-99, without trailing comma and space
+ *
  * Return: 0 on success
  */
 int main(void)
 {
     int x = 48;
 
-    while (x <= 57)
-    {
-        int y = 48;
+while (x <= 57)
+{
+int y = 48;
 
-        while (y <= 57)
-        {
-            if (x == y)
-            {
-                y++;
-                continue;
-            }
-            if ((((char)x * 10 + (char)y) - ((char)y * 10 + (char)x)) / 9 > 0)
-            {
-                y++;
-                continue;
-            }
-            putchar(x);
-            putchar(y);
-            /* Check if it's the last pair of numbers before printing comma and space */
-            if (!(x == 56 && y == 57))
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            y++;
-        }
-        x++;
-    }
-    return (0);
+while (y <= 57)
+{
+if (x == y)
+{
+y++;
+continue;
+}
+if ((((char)x * 10 + (char)y) - ((char)y * 10 + (char)x)) / 9 > 0)
+{
+y++;
+continue;
+}
+putchar(x);
+putchar(y);
+/* to preveme the last comma and space*/
+if (!(x == 56 && y == 57))
+{
+putchar(',');
+putchar(' ');
+}
+y++;
+}
+x++;
+}
+putchar(10);
+return (0);
 }
