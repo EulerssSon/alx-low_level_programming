1af3fbec6a8325759bc1f384636f890eec806153
EulerssSon <alfy.kerolous@gmail.com>
2023-09-09 11:44:54 -0700

    new task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
  * main - main function
  *
  * Return: 0 on success
  *
- * Description: print unique numbers from 0-99
+ * Description: print unique from 0-99
  */
 int main(void)
 {
-    int x = 48;
+int x = 48;
 
-    while (x <= 57)
-    {
-        int y = 48;
+while (x <= 57)
+{
+int y = 48;
 
-        while (y <= 57)
-        {
-            if (x == y)
-            {
-                y++;
-                continue;
-            }
-            if (((char)x * 10 + (char)y) - ((char)y * 10 + (char)x) / 9 > 0)
-            {
-                y++;
-                continue;
-            }
-            if (x == 56 && y == 57)
-            {
-                putchar(x);
-                putchar(y++);
-            }
-            else
-            {
-                putchar(x);
-                putchar(y++);
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        x++;
-    }
-    return (0);
+while (y <= 57)
+{
+if (x == y)
+{
+y++;
+continue;
 }
-
+if((( (char) x * 10 + (char) y) -((char) y  * 10 + (char) x)) / 9 > 0)
+{
+y++;
+continue;
+}
+if (x == 56 && y == 57)
+{
+putchar(x);
+putchar(y++);
+}
+else
+{
+putchar(x);
+putchar(y++);
+putchar(',');
+putchar(' ');
+}
+}
+x++;
+}
+return (0);
+}
