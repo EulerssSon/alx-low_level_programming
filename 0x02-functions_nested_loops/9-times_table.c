66fe8938d5329995a159935c179b6143e88127bf
EulerssSon <alfy.kerolous@gmail.com>
2023-09-13 13:15:34 -0700

    new task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * times_table - print time table
+ * Return: null just printing
+ * Description: print the multi table form 0 - 9 c under colums
+ */
+
+void times_table(void)
+{
+	int i;
+	int multiplication;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		int j;
+
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			multiplication = i * j;
+
+			if (multiplication <= 9)
+			{
+				_putchar(',')
+			}
+			else
+			{
+				_putchar((_putchar(multiplication / 10)) + 48);
+			}
+			_putchar((multiplication % 10) + 48);
+		}
+		putchar(10);
+	}
+}
+
