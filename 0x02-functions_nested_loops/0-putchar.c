ba2e49a8c665e722b2bf7bc03165bc51b2ae194c
EulerssSon <alfy.kerolous@gmail.com>
2023-09-12 19:27:01 -0700

    new task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
  #include "main.h"
+
  /**
   * main - main function
   *
   * Return: 0 on success, 1 on failure
   *
   * Description: print _putchar
- */
+  */
  int main(void)
  {
  char str[] = "_putchar";
- int i; 
+ int i;
+
  for (i = 0; i < 8; i++)
  {
- _putchar(str[i]);
+ putchar(str[i]);
  }
  _putchar('\n');
  return (0);
