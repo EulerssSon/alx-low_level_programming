3bd3a871f1f1f393419775e572e72500141fb95f
EulerssSon <alfy.kerolous@gmail.com>
2023-09-13 06:04:38 -0700

    new task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - take an int and give an int
+ * @n - first opernad
+ *
+ * Description: num + word if n >0 0 0 if 0 -1 - n<0
+ * Return: 1 , 0 , -1 , + , 0 ,-
+ */
+int print_sign(int n);
+{
+if (n > 0)
+{
+	_putchar(43);
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar(45);
+	return (-1);
+}
+else
+{
+	_putchar(43);
+	return (48);
+}
+}
