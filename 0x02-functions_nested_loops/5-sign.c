75be92592d44173405f26b05f7bed41afd5cdf4d
EulerssSon <alfy.kerolous@gmail.com>
2023-09-13 06:07:20 -0700

    new task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 
 /**
  * print_sign - take an int and give an int
- * @n - first opernad
+ * @n - first opernad to be ckecked
  *
  * Description: num + word if n >0 0 0 if 0 -1 - n<0
  * Return: 1 , 0 , -1 , + , 0 ,-
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-	_putchar(43);
-	return (1);
-}
-else if (n < 0)
-{
-	_putchar(45);
-	return (-1);
-}
-else
-{
-	_putchar(43);
-	return (48);
-}
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(43);
+		return (48);
+	}
 }
