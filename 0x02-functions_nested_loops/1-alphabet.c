fdc4a7231e787a10cf3c619ee09a65df2261fc72
EulerssSon <alfy.kerolous@gmail.com>
2023-09-12 19:39:50 -0700

    new task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_alphabet- print_alphabet function
+  *
+  * Return: alphabets
+  *
+  * Description: print alphabets
+ */
+
+void print_alphabet(void)
+{
+int ch;
+for ( ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar(10);
+return (0);
+}
