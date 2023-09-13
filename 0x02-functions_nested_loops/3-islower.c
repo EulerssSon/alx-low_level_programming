3a5c5f0978e6086b281abf5092bb16109f47f959
EulerssSon <alfy.kerolous@gmail.com>
2023-09-12 19:55:50 -0700

    new task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
  #include "main.h"
  /**
-  * islower - islower function
+  * _islower - islower function
   *
-  * Return: 0 on success, 1 on failure
+  * Return: 1 on success, 0 on failure
   *
   * Description: find lower or not
- */
+  */
 int _islower(int c)
 {
-if (c >= 97 && c <=122)
+if (c >= 97 && c <= 122)
 return (1);
 else
 return (0);
