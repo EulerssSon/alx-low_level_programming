afb4e93138033cebd198eebaa195b14765058888
EulerssSon <alfy.kerolous@gmail.com>
2023-09-13 09:47:45 -0700

    new task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,24 @@
  * print_last_digit - a function to extract the last digit
  * @n: first operant to be extracted from
  *
- * Return: retun (n%10)
+ * Return: retun a if +ve (n%10) b if -ve return +ve then % 10
  * Description: get the num %10 and return it
  */
 
 int print_last_digit(int n)
 {
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int a = n % 10;
+	int b = (-1 * n) % 10;
+
+	if (n >= 0)
+	{
+		_putchar(a + '0')
+		return (a);
+	}
+	else
+	{
+		_putchar(b + '0')
+		return (b);
+	}
 }
 
