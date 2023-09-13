379f6c10117e8fe52f2ea672fa485baacd8ce601
EulerssSon <alfy.kerolous@gmail.com>
2023-09-12 19:22:13 -0700

    new task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "main.h"
-/**
- * main - main function
- *
- * Return: 0 on success, 1 on failure
- *
- * Description: print _putchar 
-*/
-int main(void)
-{
-char str[] = "_putchar";
-int i =0
-for (; i < 8; i++)
-{
-_putchar(str[i]);
-}
-_putchar('\n');
-return (0);
-}
+ #include "main.h"
+ /**
+  * main - main function
+  *
+  * Return: 0 on success, 1 on failure
+  *
+  * Description: print _putchar
+ */
+ int main(void)
+ {
+ char str[] = "_putchar";
+ int i; 
+ for (i = 0; i < 8; i++)
+ {
+ _putchar(str[i]);
+ }
+ _putchar('\n');
+ return (0);
+ }
