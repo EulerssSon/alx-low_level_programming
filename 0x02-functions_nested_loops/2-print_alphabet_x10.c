bfff4df57b349debf37a5410c45e115daeb39dc2
EulerssSon <alfy.kerolous@gmail.com>
2023-09-12 19:45:54 -0700

    new task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_alphabet_x10- print_alphabet function
+  *
+  * Return: alphabets 10 times
+  *
+  * Description: print alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+int ch;
+int i;
+
+for(i = 0; i < 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar(10);
+}
+return;
+}
