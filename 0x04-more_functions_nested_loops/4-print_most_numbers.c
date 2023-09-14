a14240ecc2be91ce2f3f29b4217a831bcce76a34
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 16:00:51 -0700

    new task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print_numbers expcet 2 4
+ *
+ * Return: Null void function
+ *
+ * Description: print_numbers form 0 to 9 expcet 2 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if(i == 50 || i == 52)
+			continue;
+		_putchar(i);
+	}
+	_putchar(10);
+}
