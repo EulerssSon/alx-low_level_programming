5af8a359c40ddd44beccb6e19af6fdc6b0371f82
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 15:35:59 -0700

    new task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - function to print_numbers
+ *
+ * Return: Null void function
+ *
+ * Description: print_numbers form 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
