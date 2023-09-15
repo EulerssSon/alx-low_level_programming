9071101fb6493cd9ed43d5d3fbb6b3b4fad7ac50
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 17:26:55 -0700

    new task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,16 +3,15 @@
 /**
  * _isdigit - function to check if the letter is digit or not
  * @c: first opeant the char to be checked
- *
  * Return: 1 or 0
  * Description: 1 means sucess the char is a num 0 is the else one
  */
 
 int _isdigit(int c)
 {
-    if (c >= 48 && c <= 57)
-        return (1);
-	else
-    	return (0);
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
 }
 
