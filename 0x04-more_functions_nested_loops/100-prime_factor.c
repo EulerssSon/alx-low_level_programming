f9eed729a4dcad104e15f002707c9c854e5c756c
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 19:48:46 -0700

    new task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 on sucess 1 on failure
+ * Description: print the largest factore of a num
+ */
+
+int main(void)
+{
+	unsigned long int n = 0;
+	unsigned long int i;
+	unsigned long int j;
+	unsigned long int Larg_Prime_Factor = 0;
+	int TheBoolean;
+
+	for (i = 2; i <= n; i++)
+	{
+		if ((n % i) == 0)
+		{
+			TheBoolean = 1;
+
+			for (j =2; j < i / 2 ; j++)
+			{
+				if (i % j == 0)
+				{
+					TheBoolean = 0;
+					break;
+				}
+			}
+			if (TheBoolean == 1)
+			{
+				Larg_Prime_Factor = i;
+			}
+
+		}
+
+	}
+	printf("%lu\n", Larg_Prime_Factor);
+	return (0);
+}
