02feddbc13e29fe895ce36ebacdaa06f0d1cbc6c
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 19:03:26 -0700

    new task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print_diagonal
+ * @n: n for number of lines
+ *
+ * Return: Null void
+ * Description: print lines of dashess if n >= 0 depending on n
+ */
+
+void print_diagonal(int n)
+{
+		int i;
+		int j;
+
+	if (n > 0)
+	{
+
+		for (i = 1; i <= n; i++)
+		{
+			for(j = 1; j <=i - 1; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar(10);
+}
+
