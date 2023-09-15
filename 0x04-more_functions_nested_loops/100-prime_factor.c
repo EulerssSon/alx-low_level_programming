6f965138a410300cf4622a6932318884d18e1f87
EulerssSon <alfy.kerolous@gmail.com>
2023-09-15 01:41:24 -0700

    new task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,33 +10,32 @@
 
 int main(void)
 {
-	long int num;
+	long int n;
 	long int max;
 	long int i;
 
-	num = 612852475143;
+	n = 612852475143;
 	max = -1;
 
-	while (num % 2 == 0)
+	while (n % 2 == 0)
 	{
 		max = 2;
-		num /= 2;
+		n /= 2;
 	}
 
-	for (i = 3; i <= sqrt(num); i = i + 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
 			max = i;
-			num /= i;
+			n = n / i;
 		}
 	}
 
-	if (num > 2)
-		max = num;
+	if (n > 2)
+		max = n;
 
 	printf("%ld\n", max);
 
 	return (0);
 }
-
