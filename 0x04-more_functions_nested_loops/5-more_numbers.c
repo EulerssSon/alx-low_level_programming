eafb64e41b9a19138ef519eeb429afe8d0010e01
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 18:51:39 -0700

    new task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,27 @@
 #include "main.h"
-
 /**
-  * more_numbers - function
-  * Return: null
-  * Description: i hate betty
-  */
+ * more_numbers - function to print num
+ * Return: null
+ * Description: we all hate betty + checket
+ */
+
 void more_numbers(void)
 {
-int k;
-int j;
-for (k = 1; k <= 10; k++)
-{
-int i;
-for (i = 48; i <= 57; i++)
-{
-_putchar(i);
+	int k;
+	int j;
+	int i;
+
+	for (k = 1; k <= 10; k++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			_putchar(i);
+		}
+		for (j = 0; j <= 4; j++)
+		{
+			_putchar(49);
+			_putchar(j + '0');
+		}
+	}
 }
-for (j = 0; j <= 4; j++)
-{
-_putchar(49);
-_putchar(j + '0');
-}
-_putchar(10);
-}
-}
+
