e988925b1a8e54e45f33d54f3cd15d5779f813cc
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 19:13:14 -0700

    new task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_square - print sqaure of *
@@ -21,10 +20,10 @@ void print_square(int size)
 			{
 				_putchar('#');
 			}
-			putchar(10);
+			_putchar(10);
 		}
 	}
 	else
-		putchar(10);
+		_putchar(10);
 }
 
