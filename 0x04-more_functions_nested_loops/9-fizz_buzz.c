776ade99560a5e1ddd59fba6f6c44f5533d8d8ee
EulerssSon <alfy.kerolous@gmail.com>
2023-09-14 18:26:25 -0700

    new task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main fucntion
+ *
+ * Return: 0 on sucess 1 on fail
+ *
+ * Description: Fizz_Buzz game if n %3 onlt fizz % 5 only buzz %15 Fizz_Buzz
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
+
