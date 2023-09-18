9c9c66241a91e51d3934993c5281619e8cc01841
EulerssSon <alfy.kerolous@gmail.com>
2023-09-18 12:28:19 -0700

    new task i am so tired

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
+
