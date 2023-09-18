d04aca47b69f93c3e763116ede451494ab257314
EulerssSon <alfy.kerolous@gmail.com>
2023-09-18 15:22:49 -0700

    passing arr int o a function

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array of integers
+ * @n: number of elements to be printed
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[i]);
+}
