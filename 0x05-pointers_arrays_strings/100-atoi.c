a6a8732cb7a2eac6f46c44146e95b99db3b9253b
EulerssSon <alfy.kerolous@gmail.com>
2023-09-18 17:40:21 -0700

    my homemade atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: the integer value of the string
+ */
+
+int _atoi(char *s)
+{
+	int n = 0, sign = 1;
+
+	while (!(*s >= '0' && *s <= '9'))
+	{
+		s++; /*escaping all non numbers*/
+	}
+	s--; /*going back to check the sigh*/
+	if (*s == '-')
+	{
+		sign = -1;
+	}
+	s++; /*going back to the first number*/
+	while (*s >= '0' && *s <= '9')
+	{
+		n = n * 10 + (*s - '0');
+		s++;
+	}
+	return (n * sign);
+}
