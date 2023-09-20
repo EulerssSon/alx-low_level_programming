a31b8744a8ae071476a3239a0f7b783ed9cedca7
EulerssSon <alfy.kerolous@gmail.com>
2023-09-20 16:48:09 -0700

    new task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,40 +1,25 @@
-#include "main.h"
-
-/**
- * _strcmp - compares two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: difference between the two strings
- */
 int _strcmp(char *s1, char *s2)
 {
-	int length1 = 0, length2 = 0, i = 0;
+    int i = 0;
 
-	while (s1[length1] != '\0')
-	{
-		length1++;
-	}
-	while (s2[length2] != '\0')
-	{
-		length2++;
-	}
-	if (length1 < length2)
-		return (s1[0] - s2[0]);
-	else if (length1 > length2)
-		return (s1[0] - s2[0]);
-	while (i < length1)
-	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-		{
-			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
-			else
-				return (s2[i] - s1[i]);
-		}
-	}
-	return (0);
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] != s2[i]) {
+            return s1[i] - s2[i];
+        }
+        i++;
+    }
+
+    // If both strings have reached the end ('\0'), they are equal
+    if (s1[i] == '\0' && s2[i] == '\0') {
+        return 0;
+    }
+
+    // If only s1 has reached the end, it is smaller than s2
+    if (s1[i] == '\0') {
+        return -1;
+    }
+
+    // If only s2 has reached the end, it is smaller than s1
+    return 1;
 }
 
