5b7de852a42964e1fcb7e34d6919bbaa9b93d6c4
EulerssSon <alfy.kerolous@gmail.com>
2023-09-20 19:48:53 -0700

    new task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+
+char *leet(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s == 'a' || *s == 'A')
+			*s = '4';
+		else if (*s == 'e' || *s == 'E')
+			*s = '3';
+		else if (*s == 'o' || *s == 'O')
+			*s = '0';
+		else if (*s == 't' || *s == 'T')
+			*s = '7';
+		else if (*s == 'l' || *s == 'L')
+			*s = '1';
+		s++;
+	}
+	return (s);
+}
+
