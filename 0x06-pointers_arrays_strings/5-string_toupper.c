4f8314061b23fd0698245a970950ba8bccbf6a65
EulerssSon <alfy.kerolous@gmail.com>
2023-09-20 17:31:26 -0700

    Turning lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string to be changed
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) -= 32; /*difference between small and captial in ascii is 32*/
+		}
+		i++;
+	}
+	return (s);
+}
