07741329e0cc90183e2fb2ab361d444c96a4dfd9
EulerssSon <alfy.kerolous@gmail.com>
2023-09-20 18:36:55 -0700

    strcmp functino in c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference between the two strings
+ */
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	int i;
 
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] != s2[i]) {
-            return s1[i] - s2[i];
-        }
-        i++;
-    }
-
-    // If both strings have reached the end ('\0'), they are equal
-    if (s1[i] == '\0' && s2[i] == '\0') {
-        return 0;
-    }
-
-    // If only s1 has reached the end, it is smaller than s2
-    if (s1[i] == '\0') {
-        return -1;
-    }
-
-    // If only s2 has reached the end, it is smaller than s1
-    return 1;
+	for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
-
