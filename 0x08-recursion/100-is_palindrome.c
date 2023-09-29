0a31a8d44a20d8c591fa40fce31b95c83669bc54
EulerssSon <alfy.kerolous@gmail.com>
2023-09-28 19:17:52 -0700

    removing loops

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,36 +1,58 @@
 #include "main.h"
 
 /**
- * check_palindrome - checks if a string is a palindrome
+ * rec_length - returns the length of a string
  * @s: string to check
- * @len: length of string
+ *
+ * Return: length of string
+ */
+
+int rec_length(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + rec_length(s + 1));
+}
+
+/**
+ * rec_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ * @left: leftmost index
+ * @right: rightmost index
  *
  * Return: 1 if palindrome, 0 otherwise
  */
 
-int check_palindrome(char *s, int len)
+int rec_palindrome(char *s, int left, int right)
 {
-	if (len <= 1)
+	if (left >= right)
+	{
 		return (1);
-	else if (*s == *(s + len - 1))
-		return (check_palindrome(s + 1, len - 2));
-	else
+	}
+	if (s[left] != s[right])
+	{
 		return (0);
+	}
+	return (rec_palindrome(s, left + 1, right - 1));
 }
 
 /**
- * is_palindrome - function to ckeck
+ * is_palindrome - checks if a string is a palindrome
  * @s: string to check
+ *
  * Return: 1 if palindrome, 0 otherwise
  */
 
 int is_palindrome(char *s)
 {
-	int len = 0;
+	int len = rec_length(s);
 
-	while (*(s + len))
-		len++;
-
-	return (check_palindrome(s, len));
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	return (rec_palindrome(s, 0, len - 1));
 }
 
