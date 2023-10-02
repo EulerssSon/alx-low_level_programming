129f91ef3bb450595b7c235be47d1202a9b418b7
EulerssSon <alfy.kerolous@gmail.com>
2023-10-02 09:30:39 -0700

    fixing betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints the minimum number of coins to make for an amount of money
+ * main - prints the minimum number of coins to make change for money
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
@@ -15,12 +16,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
-	if (cents <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+
 	while (cents > 0)
 	{
 		if (cents >= 25)
@@ -35,6 +33,9 @@ int main(int argc, char *argv[])
 			cents -= 1;
 		coins++;
 	}
+
 	printf("%d\n", coins);
+
 	return (0);
 }
+
