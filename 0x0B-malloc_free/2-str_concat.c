4306b4c47ad19255ee5249649ba292cafad77470
EulerssSon <alfy.kerolous@gmail.com>
2023-10-03 14:35:44 -0700

    fixing null convert to white space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 	char *concat;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
+	if (s1 == NULL)
+		len1 = 0;
+	else
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			;
 	if (s2 == NULL)
 		len2 = 0;
 	else
@@ -24,17 +27,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (concat == NULL)
 		return (NULL);
-
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		concat[len1] = s1[len1];
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			concat[len1] = s1[len1];
+	}
 	if (s2 != NULL)
 	{
 		for (len2 = 0; s2[len2] != '\0'; len2++)
 			concat[len1 + len2] = s2[len2];
-		concat[len1 + len2] = '\0';
 	}
-	else
-		concat[len1] = '\0';
+	concat[len1 + len2] = '\0';
 	return (concat);
 }
 
