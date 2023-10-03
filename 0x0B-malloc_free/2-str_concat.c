d5d6d8f137ae5739fc98e90d7d258b2f276a053e
EulerssSon <alfy.kerolous@gmail.com>
2023-10-03 14:07:53 -0700

    string concatnation 2 strings with new variable dynamically allocated

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2;
+	char *concat;
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	concat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		concat[len1] = s1[len1];
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		concat[len1 + len2] = s2[len2];
+	concat[len1 + len2] = '\0';
+
+	return (concat);
+}
+
