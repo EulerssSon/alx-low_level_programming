8beed342a6d17ba09dd4586327601985e1e79d11
EulerssSon <alfy.kerolous@gmail.com>
2023-10-03 14:11:44 -0700

    fixing empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,8 @@ char *str_concat(char *s1, char *s2)
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		concat[len1] = s1[len1];
+	if (str == NULL)
+		s2 = "";
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		concat[len1 + len2] = s2[len2];
 	concat[len1 + len2] = '\0';
