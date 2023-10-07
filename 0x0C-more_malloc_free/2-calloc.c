09a187ff54d985cca0438e9571f40703d65115d4
EulerssSon <alfy.kerolous@gmail.com>
2023-10-06 17:22:58 -0700

    making the function calloc using alloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of elements
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *arr, i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = 0;
+	return (arr);
+}
+
