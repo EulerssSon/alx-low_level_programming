81f3121acd2cc401f3f4c165fa337535dcf2f2b3
EulerssSon <alfy.kerolous@gmail.com>
2023-10-24 15:01:33 -0700

    most of advancde done

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the size of the list that was freed
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *temp;
+	size_t count = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+
+	while (current != NULL)
+	{
+		count++;
+		if (current <= current->next)
+		{
+			free(current);
+			break;
+		}
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*h = NULL;
+	return (count);
+}
+
