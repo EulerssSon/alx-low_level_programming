dc3057b7027799ada0dec9987b142a602238db69
EulerssSon <alfy.kerolous@gmail.com>
2023-11-25 22:14:23 -0800

    null case forget to check fixed and returned 0 in the 0-file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -30,6 +30,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	int sum = 0, i = _strlen(b) - 1, j = 0;
 
+	if (b == NULL)
+		return (0);
 	for (; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
