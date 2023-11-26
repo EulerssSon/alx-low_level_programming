51d8f8fc4e93e470b40b4d0c10e8ddc578d7c341
EulerssSon <alfy.kerolous@gmail.com>
2023-11-25 22:09:01 -0800

    files needed are all added + made the print bin func

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a string
+ *
+ * Return: length of a string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int sum = 0, i = _strlen(b) - 1, j = 0;
+
+	for (; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		sum += (b[i] - '0') * (1 << j);
+		j++;
+	}
+	return (sum);
+}
+
