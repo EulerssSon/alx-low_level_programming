6c204b39fad8ee1a9d4ac6daa0a96b74b67877d2
EulerssSon <alfy.kerolous@gmail.com>
2023-11-27 14:27:45 -0800

    appednd to ftext file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _str_len - returns the length of a string
+ * @str: string to be checked
+ *
+ * Return: length of the string
+ */
+int _str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wr, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
+		return (-1);
+	len = _str_len(text_content);
+
+	if (text_content != NULL)
+		wr = write(fd, text_content, len);
+
+	if (wr < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
+
