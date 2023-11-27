a95d480a6718cfdd1c597b7bd61115b0dc112c56
EulerssSon <alfy.kerolous@gmail.com>
2023-11-27 13:44:56 -0800

    first fucntion added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t fd, readSize;
+	char buf[1024 * 8];
+
+
+	if (filename == NULL || letters == 0)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == (unsigned long int)-1)
+		return (0);
+
+	readSize = read(fd, buf, letters);
+	readSize = write(STDOUT_FILENO, buf, letters);
+
+	close(fd);
+
+	return (readSize);
+}
+
