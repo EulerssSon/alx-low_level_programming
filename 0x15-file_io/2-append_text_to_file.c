eea2f463adc775e1eed5825e831313a37dcf9f25
EulerssSon <alfy.kerolous@gmail.com>
2023-11-30 17:44:18 -0800

    fixing a Segmatntiatiojn Fault str len was not chcecking if it was null or not

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,7 +32,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	len = _str_len(text_content);
+	if (text_content != NULL)
+		len = _str_len(text_content);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
